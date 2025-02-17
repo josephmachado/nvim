89b1b4115545b4074141f7fdbb536319415cbf8c
JosephKevinMachado <joseph.machado.professional@gmail.com>
2025-02-17 10:29:06 -0500

    2025-02-17-10-29-06

---
+++ lua/custom/plugins/neogen.lua
@@ -0,0 +1,10 @@
+return {
+  'danymat/neogen',
+  config = function()
+    require('neogen').setup {}
+    local opts = { noremap = true, silent = true }
+    vim.api.nvim_set_keymap('n', '<Leader>nf', ":lua require('neogen').generate()<CR>", opts)
+  end,
+  -- Uncomment next line if you want to follow only stable versions
+  -- version = "*"
+}
