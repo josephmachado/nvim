b0e48c5449652589a315b3b7018ba474b072b4ce
JosephKevinMachado <joseph.machado.professional@gmail.com>
2025-04-04 07:14:44 -0400

    2025-04-04-07-14-44

--- init.lua
+++ init.lua
@@ -1045,3 +1045,35 @@ vim.g.vmt_fence_text = 'TOC' -- Custom text for the HTML comment fence
 -- For init.lua
 vim.o.foldmethod = 'indent'
 vim.o.foldlevel = 99
+
+-- Defer to pyright for textDocument/hover
+-- After the servers are initialized, add your autocmd
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client == nil then
+      return
+    end
+    if client.name == 'ruff' then
+      -- Disable hover in favor of Pyright
+      client.server_capabilities.hoverProvider = false
+    end
+  end,
+  desc = 'LSP: Disable hover capability from Ruff',
+})
+
+require('lspconfig').pyright.setup {
+  settings = {
+    pyright = {
+      -- Using Ruff's import organizer
+      disableOrganizeImports = true,
+    },
+    python = {
+      analysis = {
+        -- Ignore all files for analysis to exclusively use Ruff for linting
+        ignore = { '*' },
+      },
+    },
+  },
+}
