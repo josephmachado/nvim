7ac53ac2a3d24d339af4e30778e37ccbdd5ea774
JosephKevinMachado <joseph.machado.professional@gmail.com>
2025-04-06 15:27:28 -0400

    2025-04-06-15-27-28

--- lua/custom/plugins/neogen.lua
+++ lua/custom/plugins/neogen.lua
@@ -1,7 +1,19 @@
 return {
   'danymat/neogen',
   config = function()
-    require('neogen').setup {}
+    require('neogen').setup {
+      enabled = true,
+      languages = {
+        python = {
+          template = {
+            annotation_convention = 'numpydoc', -- or "google" or "numpydoc"
+            use_lsp_format = true, -- This is key - tells Neogen to use LSP type info
+            include_parameters_type = true, -- Ensures parameter types are included
+            include_return_type = true, -- Ensures return types are included
+          },
+        },
+      },
+    }
     local opts = { noremap = true, silent = true }
     vim.api.nvim_set_keymap('n', '<Leader>nf', ":lua require('neogen').generate()<CR>", opts)
   end,
