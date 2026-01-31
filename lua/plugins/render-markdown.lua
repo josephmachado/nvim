81c8ee2fefda5402c8f46a81dd2c1d139e8001c9
JosephKevinMachado <joseph.machado.professional@gmail.com>
2026-01-31 13:20:11 -0500

    2026-01-31-13-20-11

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -2,4 +2,7 @@ return {
   "MeanderingProgrammer/render-markdown.nvim",
   ft = { "markdown", "codecompanion", "quarto", "qmd" },
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },
+  opts = {
+    enabled = true,
+  },
 }
