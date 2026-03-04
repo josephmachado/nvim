1c9ed3e6d29690f71e247e6467de6ab848d5604f
JosephKevinMachado <joseph.machado.professional@gmail.com>
2026-03-04 06:35:55 -0500

    2026-03-04-06-35-55

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,8 +3,30 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "html", "cssls", "pyright"}
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- Ruff setup with format on save
+vim.lsp.config.ruff = {
+  on_attach = function(client, bufnr)
+    -- Format on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ async = false })
+      end,
+    })
+  end,
+}
+vim.lsp.enable("ruff")
 
+-- Pyright config - disable organize imports since Ruff handles it
+vim.lsp.config.pyright = {
+  settings = {
+    pyright = {
+      disableOrganizeImports = true,
+    },
+  },
+}
+
+-- Harper LS
 vim.lsp.config.harper_ls = {
   filetypes = { "markdown", "qmd", "txt", "python", "sh", "quarto" },
 }
